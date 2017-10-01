fc57de14bf3a27cc1429ec8f40eaca91325bbe8e
André Kuhlmann <akuhltime@gmail.com>
2017-10-01 16:26:04 +0200

    Initial Commit

---
+++ KeyRemapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyRemapper
+//
+//  Created by André Kuhlmann on 01.10.17.
+//  Copyright © 2017 André Kuhlmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeyRemapper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeyRemapper
+//
+//  Created by André Kuhlmann on 01.10.17.
+//  Copyright © 2017 André Kuhlmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
